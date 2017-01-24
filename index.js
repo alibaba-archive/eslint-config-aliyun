041ac34c5fa10dd31f59096097084bb21159c72e
HQidea <hqidea@yahoo.com>
2017-01-24 17:19:01 +0800

    feature: add new rules

--- index.js
+++ index.js
@@ -1,40 +1,183 @@
 module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": 'module',
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "jsx": true,
+      "experimentalObjectRestSpread": true
+    }
+  },
   "env": {
     "browser": true,
+    "node": true,
     "commonjs": true,
     "es6": true,
-    "node": true
+    "mocha": true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
-    },
-    "sourceType": "module"
+  "globals": {
+    "load": true
   },
   "plugins": [
     "react"
   ],
   "rules": {
+    /* Best Practices */
+    "array-callback-return": [
+      "error"
+    ],
+    "no-useless-escape": [
+      "error"
+    ],
+    "eqeqeq": [
+      "error",
+      "always"
+    ],
+    "radix": [
+      "error"
+    ]
+    /* Stylistic Issues */
     "indent": [
       "error",
-      2
+      2,
+      {
+        "SwitchCase": 1
+      }
     ],
-    "linebreak-style": [
+    "space-before-blocks": [
+      "error"
+    ],
+    "keyword-spacing": [
+      "error"
+    ],
+    "space-infix-ops": [
+      "error"
+    ],
+    "eol-last": [
+      "error"
+    ],
+    "padded-blocks": [
       "error",
-      "unix"
+      "never"
+    ],
+    "space-in-parens": [
+      "error",
+      "never"
+    ],
+    "array-bracket-spacing": [
+      "error",
+      "never"
+    ],
+    "object-curly-spacing": [
+      "error",
+      "never"
+    ],
+    "computed-property-spacing": [
+      "error",
+      "never"
+    ],
+    "block-spacing": [
+      "error",
+      "never"
+    ],
+    "key-spacing": [
+      "error"
+    ],
+    "no-trailing-spaces": [
+      "error"
+    ],
+    "func-call-spacing": [
+      "error"
+    ],
+    "comma-spacing": [
+      "error"
+    ],
+    "semi-spacing": [
+      "error"
+    ],
+    "space-before-function-paren": [
+      "error",
+      {
+        "anonymous": "always",
+        "named": "never"
+      }
+    ],
+    "space-unary-ops": [
+      "error",
+      {
+        "words": true,
+        "nonwords": false
+      }
+    ],
+    "no-multiple-empty-lines": [
+      "error"
+    ],
+    "semi": [
+      "error",
+      "always"
     ],
     "quotes": [
       "error",
       "single"
     ],
-    "semi": [
+    "quote-props": [
       "error",
-      "always"
+      "as-needed"
+    ],
+    "brace-style": [
+      "error"
+    ],
+    "new-parens": [
+      "error"
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "operator-linebreak": [
+      "error",
+      "after"
+    ],
+    "one-var": [
+      "error",
+      "never"
+    ],
+    "no-unneeded-ternary": [
+      "error"
+    ],
+    "camelcase": [
+      "error"
+    ],
+    "new-cap": [
+      "error"
+    ],
+    "no-new-object": [
+      "error"
+    ],
+    "no-array-constructor": [
+      "error"
+    ],
+    /* ECMAScript 6 */
+    "arrow-parens": [
+      "error",
+      "as-needed",
+      {
+        "requireForBlockBody": true
+      }
+    ],
+    "arrow-spacing": [
+      "error"
+    ],
+    "template-curly-spacing": [
+      "error"
+    ],
+    "no-duplicate-imports": [
+      "error"
+    ],
+    "generator-star-spacing": [
+      "error",
+      "after"
     ]
   },
-  "globals": {
-    "load": true
-  }
+  "extends": "eslint:recommended"
 };
